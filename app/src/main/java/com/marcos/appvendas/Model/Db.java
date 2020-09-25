f51d61deb2e2ff71a00e29d8074ab95e58bc03c8
Marcos Roberto Silva <marcosrsilva97@hotmail.com>
2020-09-25 09:25:00 -0300

    Criação da tabela de produtos

--- app/src/main/java/com/marcos/appvendas/Model/Db.java
+++ app/src/main/java/com/marcos/appvendas/Model/Db.java
@@ -8,7 +8,7 @@ public class Db extends SQLiteOpenHelper {
 
     public static String DbName = "Vendas.db";
     public static int version = 1;
-    private static String query ="CREATE TABLE [CLIENTES](" +
+    private static String tblClientes ="CREATE TABLE [CLIENTES](" +
             "cli_codigo INTEGER," +
             "cli_nome VARCHAR DEFAULT 50," +
             "cli_fantasia, cli_endereco VARCHAR DEFAULT 50," +
@@ -22,6 +22,15 @@ public class Db extends SQLiteOpenHelper {
             "cli_email DEFAULT VARCHAR 50," +
             "cli_enviado DEFAULT CHAR 1," +
             "cli_chave DEFAULT VARCHAR 100)";
+
+    private String tblProdutos = "CREATE TABLE [PRODUTOS] (" +
+            "prd_codigo, INTEGER" +
+            "prd_EAN13, VARCHAR DEFAULT 13" +
+            "prd_descricao, VARCHAR DEFAULT 50" +
+            "prd_unMedida, VARCHAR DEFAULT 10" +
+            "prd_custo, DECIMAL(10,2)" +
+            "prd_preco,DECIMAL(10,2)" +
+            "prd_categoria VARCHAR DEFAULT 30)";
 
     public Db(Context context){
         super(context, DbName,null,version);
